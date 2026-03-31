1da0f4323f8b8242667bb22170b42ccabc99e696
kanakkholwal <kanakkholwal@gmail.com>
2026-03-31 22:32:47 +0530

    chore: initialize build configurations and TypeScript project files across all packages

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    sourcemap: true,
+    splitting: false,
+    clean: true,
+    dts: true,
+})
+
