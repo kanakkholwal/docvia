a1d64f459fa0b8c6ae1992c20832d323d760e652
kanakkholwal <kanakkholwal@gmail.com>
2026-03-31 22:32:47 +0530

    chore: initialize build configurations and TypeScript project files across all packages

--- packages/ir/tsup.config.ts
+++ packages/ir/tsup.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/transform.ts'],
-  format: ['esm'],
-  dts: true,
   sourcemap: true,
   splitting: false,
   clean: true,
-  shims: true,
-  minify: false,
+  dts: true,
 });
