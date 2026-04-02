b77e1225a3f60b86ad4c07cdcc4503c34c06f7e1
Kanak <kanakkholwal@gmail.com>
2026-04-02 16:28:36 +0530

    feat: remove unused type definitions and interfaces from IR and renderer-core packages

--- packages/renderer-core/src/errors.d.ts
+++
@@ -1,6 +0,0 @@
-import type { IRNode } from "@docvia/ir";
-export declare class RenderError extends Error {
-    readonly code: string;
-    readonly node: IRNode;
-    constructor(code: string, message: string, node: IRNode);
-}
